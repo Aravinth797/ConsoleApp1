8e2d6adfb018d559996f85cd3cbe29cf347672b3
16053 <16053@VA005479>
2025-08-21 12:41:57 +0530

    Now more changes from my machine..

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Aravinth!");
 Console.WriteLine("Hello, Ajay!");
+
+Console.WriteLine("Hello, Muthu!");
+Console.WriteLine("Hello, Bala!");

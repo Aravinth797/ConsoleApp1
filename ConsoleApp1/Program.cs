ab7d90313309d1bd6ef6cdaf54a12a73bdd7c8d0
Aravinth <119647710+Aravinth797@users.noreply.github.com>
2025-08-21 12:43:35 +0530

    Kept One message in console.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Aravinth!");
-Console.WriteLine("Hello, Ajay!");
-
-Console.WriteLine("Hello, Muthu!");
-Console.WriteLine("Hello, Bala!");
